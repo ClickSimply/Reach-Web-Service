f5b8ffa86ac679fd3b5ba5b24088e10add9d1329
Scott Lott <Scott@ClickSimply.com>
2020-06-18 22:34:06 -0700

    weeeee

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 #[macro_use]
 extern crate lazy_static;
 extern crate hyper;
+use actix_rt;
 use actix_web::HttpRequest;
+use actix_web::{get, web, App, HttpServer, Responder};
+use async_recursion::async_recursion;
 use core::marker::PhantomData;
+use futures::{Future, Stream, StreamExt};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Client;
+use hyper::{Body, Request, Response, Server};
+use num_cpus;
+use quick_js::{Context, JsAsync, JsValue};
+use std::convert::Infallible;
+use std::pin::Pin;
 use std::sync::Mutex;
+use std::{
+    cell::{Cell, RefCell},
+    net::SocketAddr,
+    rc::Rc,
+    sync::{Arc, RwLock},
+    task::Waker,
+    thread::LocalKey,
+    time::SystemTime,
+};
 use tokio::net::TcpListener;
 use tokio::prelude::*;
-use quick_js::{Context, JsValue, JsAsync};
 use tokio::runtime::*;
 use tokio::time::*;
-use std::{pin::Pin, future::Future};
-use async_recursion::async_recursion;
-use std::convert::Infallible;
-use std::{sync::{RwLock, Arc}, net::SocketAddr, cell::{Cell, RefCell}, rc::Rc, thread::LocalKey, task::Waker, time::SystemTime};
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
-use num_cpus;
-use hyper::Client;
-use actix_web::{get, web, App, HttpServer, Responder};
-use actix_rt;
+use deno_core::*;
+use deno_core::ErrBox;
+use deno_core::ModuleLoader;
+use deno_core::EsIsolate;
 
 /*
 fn js_std_loop(ctx: *mut libquickjs_sys::JSContext) {
@@ -59,9 +72,7 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 
     // let ctx = Context::new().unwrap();
 
-    let host= req.uri().to_string();
-
-
+    let host = req.uri().to_string();
 
     // let result = ctx.eval_as::<String>(format!("'<h1>' + '{}'.toLowerCase() + '</h1>'", host).as_str()).unwrap();
     /*
@@ -71,9 +82,8 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
             complete('hello');
         })")
         // ctx.step();
-        
-        // println!("{:?}", promise); 
 
+        // println!("{:?}", promise);
         // let result = format!("lowerCase({})", String::from_utf8(host).unwrap());
 
         // return ctx.call_function("lowerCase", vec![host]).unwrap().into_string().unwrap();
@@ -88,30 +98,27 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 }
 
 fn test(CTX: &'static LocalKey<Context>) {
-    CTX.with(|ctx| {
-
-    });
+    CTX.with(|ctx| {});
 }
 
 async fn run() {
+    CONTEXT.with(|context| {
+        // JsAsync::init(&CONTEXT).unwrap();
 
-    CONTEXT.with(|context | {
+        context
+            .add_callback("print", |val: String| {
+                println!("{}", val);
+                return "";
+            })
+            .unwrap();
 
-        JsAsync::init(&CONTEXT).unwrap();
-
-        context.add_callback("print", |val: String| {
-            println!("{}", val);
-            return "";
-        }).unwrap();
-
-/*
+        /*
         context.eval("let timerCbs = []; const setTimeout = (cb, timeout) => {
             let len = timerCbs.length;
             timerCbs.push(cb);
             async_timers(len, timeout);
             return len;
         };").unwrap();
-        
         context.add_callback("async_timers", |index: i32, timeout: i32| {
             let time = timeout as u64;
             let idx = index;
@@ -125,7 +132,9 @@ async fn run() {
             index
         }).unwrap();*/
 
-        context.eval("const fetch = (url) => {
+        context
+            .eval(
+                "const fetch = (url) => {
                 return new Promise((res, rej) => {
                     let ln = __fetch_cbs.length;
                     __fetch_async(ln, url);
@@ -133,26 +142,39 @@ async fn run() {
                 });
             };
             const __fetch_cbs = [];
-        ").unwrap();
-        
-        context.add_callback("__fetch_async", |i: i32, url: String| {
-            tokio::task::spawn_local(async move {
-                // Await the response...
-                let body = reqwest::get(url.as_str()).await.unwrap().text().await.unwrap();
+        ",
+            )
+            .unwrap();
 
-                CONTEXT.with(|ctx| {
-                    ctx.eval(format!("__fetch_cbs[{}][0]({:?});", i, body).as_str()).unwrap();
-                    ctx.step(); // resolve promise
+        context
+            .add_callback("__fetch_async", |i: i32, url: String| {
+                tokio::task::spawn_local(async move {
+                    // Await the response...
+                    let body = reqwest::get(url.as_str())
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+
+                    CONTEXT.with(|ctx| {
+                        ctx.eval(format!("__fetch_cbs[{}][0]({:?});", i, body).as_str())
+                            .unwrap();
+                        ctx.step(); // resolve promise
+                    });
                 });
-            });
-            0i32
-        }).unwrap();
+                0i32
+            })
+            .unwrap();
 
-        context.eval("setTimeout(() => {
+        context
+            .eval(
+                "setTimeout(() => {
             print('hello');
-        }, 1000)").unwrap();
+        }, 1000)",
+            )
+            .unwrap();
         // println!("{:?}", value);
-        
     });
 
     let addr = ([127, 0, 0, 1], 3000).into();
@@ -173,12 +195,11 @@ async fn run() {
             // `service_fn` is a helper to convert a function that
             // returns a Response into a `Service`.
             Ok::<_, Error>(service_fn(move |_req: Request<Body>| {
-
                 async move {
-
-                    let str_result = JsAsync::eval_as::<String>(&CONTEXT, r####"
+                    /*let str_result = JsAsync::eval_as::<String>(&CONTEXT, r####"
                         complete(await fetch("https://google.com"));
-                    "####.to_string()).await.unwrap();
+                    "####.to_string()).await.unwrap();*/
+                    let str_result = "hello".to_owned();
 
                     Ok::<_, Error>(Response::new(Body::from(str_result)))
                     // Ok::<_, Error>(Response::new(Body::from("hello")))
@@ -197,18 +218,169 @@ async fn run() {
     }
 }
 
-async fn hello_world(req: HttpRequest, _stream: actix_web::web::Payload) -> actix_web::HttpResponse {
+/*
+thread_local!(pub static V8_RUST: V8_Runtime<'static> = {
+    let mut isolate = v8::Isolate::new(Default::default());
+
+    let mut handle_scope = v8::HandleScope::new(&mut isolate);
+    let scope = handle_scope.enter();
+    let context = v8::Context::new(scope);
+    let mut context_scope = v8::ContextScope::new(scope, context);
+    let scope = context_scope.enter();
+
+    V8_Runtime {
+        scope: RefCell::new(**scope),
+        context: RefCell::new(context)
+    }
+});*/
+
+pub struct SuperUnsafeCell<T> {
+    item: core::cell::UnsafeCell<T>
+}
+
+impl<T> SuperUnsafeCell<T> {
+    pub fn new(item: T) -> Self {
+        Self { item: core::cell::UnsafeCell::new(item) }
+    }
+
+    pub fn borrow(&self) -> &T {
+        let self_bytes = unsafe { &*self.item.get() };
+        self_bytes
+    }
+
+    pub fn borrow_mut(&self) -> &mut T {
+        let self_bytes = unsafe { &mut *self.item.get() };
+        self_bytes
+    }
+}
+
+
+async fn main_handler(
+    ctx: actix_web::web::Data<SuperUnsafeCell<EsIsolate>>,
+    req: HttpRequest,
+    mut body: actix_web::web::Payload,
+) -> actix_web::HttpResponse {
     // actix_web::HttpResponse::from("<h1>Hello, world!</h1>")
     let mut result = actix_web::HttpResponse::Ok();
-    
-    actix_web::HttpResponse::Ok().content_type("text/html").body(format!("<h1>Hello, {}</h1>", req.uri()))
+
+    let mut bytes = actix_web::web::BytesMut::new();
+    while let Some(item) = body.next().await {
+        bytes.extend_from_slice(&item.unwrap());
+    }
+
+    // println!("thread: {:?}", std::thread::current().id());
+
+    if bytes.len() > 0 {
+        println!("Req bytes: {:?}", bytes);
+    }
+
+
+    let mut js_context: &mut EsIsolate = &mut *ctx.borrow_mut();
+
+    match js_context.execute("file.js", "Deno.stdout.write(new Uint8Array(0))") {
+        Ok(x) => {
+            println!("Success {:?}", x);
+        },
+        Err(x) => {
+            println!("Error {:?}", x);
+        }
+    }
+
+
+
+    // let mut scope = V8_RUST.scope.borrow_mut();
+    // let mut context = *V8_RUST.context.borrow_mut();
+
+    // let code = v8::String::new(&mut *scope, "'Hello' + ' World!'").unwrap();
+    // println!("javascript code: {}", code.to_rust_string_lossy(scope));
+
+    // let mut script = v8::Script::compile(&mut *scope, context, code, None).unwrap();
+
+    // let result = script.run(&mut *scope, context).unwrap();
+    //  let result = result.to_string(&mut *scope).unwrap();
+    // println!("result: {}", result.to_rust_string_lossy(scope));
+
+    /*let js_result: String = ctx.eval_as(format!("(() => {{
+        let url = '{:?}';
+        return '<h1>Hello, ' + url.toLowerCase() + '</h1>';
+    }})()", req.uri()).as_str()).unwrap();*/
+
+    /*let js_context = ctx.get_ref() as *const Context;
+
+    JsAsync::eval(js_context,"setTimeout(() => {
+        complete();
+    }, 1000)".to_owned()).await.unwrap();*/
+
+    actix_web::HttpResponse::Ok()
+        .content_type("text/html")
+        .body("<h2>hello</h2>")
+    /*let js_result = JsAsync::eval_as::<String>(js_context, "complete('<h1>hello</h1>')".to_owned()).await;
+
+    match js_result {
+        Ok(x) => {
+            actix_web::HttpResponse::Ok().content_type("text/html").body(x)
+        },
+        Err(x) => {
+            println!("{:?}", x);
+            actix_web::HttpResponse::Ok().content_type("text/html").body("Error")
+        }
+    }*/
+
+    // let js_result = format!("<h1>Hello, {}</h1>", req.uri());
+}
+
+struct Null_Loader {
+
+}
+
+impl Null_Loader {
+    pub fn new() -> Self {
+        Null_Loader {}
+    }
+}
+
+impl deno_core::ModuleLoader for Null_Loader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        is_main: bool,
+      ) -> Result<ModuleSpecifier, ErrBox> {
+          todo!()
+    }
+
+    fn load(
+        &self,
+        module_specifier: &ModuleSpecifier,
+        maybe_referrer: Option<ModuleSpecifier>,
+        _is_dyn_import: bool,
+      ) -> Pin<Box<deno_core::ModuleSourceFuture>> {
+          todo!()
+      }
+}
+
+// rusty_v8::isolate::OwnedIsolate
+fn new_js_context() -> SuperUnsafeCell<EsIsolate> {
+
+    let worker = deno::worker::MainWorker::new();
+
+    let startup = deno_core::StartupData::None;
+
+    let null_loader = Rc::new(Null_Loader::new());
+
+    let mut isolate = EsIsolate::new(null_loader, startup, false);
+
+    // isolate.execute("cli.js", std::str::from_utf8(CLI_SNAPSHOT).unwrap()).unwrap();
+
+    SuperUnsafeCell::new(isolate)
 }
 
 fn main() {
     let mut single_rt = Builder::new()
-    .basic_scheduler()
-    .enable_all()
-    .build().unwrap();
+        .basic_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
 
     single_rt.spawn(async {
         //tokio::time::delay_for(Duration::from_millis(1000)).await;
@@ -224,7 +396,9 @@ fn main() {
 
         let _ = actix_web::HttpServer::new(|| {
             // actix_web::App::new().service(actix_web::web::resource("/").to(|| async { "<h1>Hello world!</h1>" }))
-            actix_web::App::new().service(actix_web::web::resource("*").to(hello_world))
+            actix_web::App::new()
+                .data(new_js_context())
+                .service(actix_web::web::resource("*").to(main_handler))
         })
         .bind("127.0.0.1:8082")
         .unwrap()
@@ -233,7 +407,6 @@ fn main() {
     });
     // local.block_on(&mut single_rt, run());
 }
- 
 // Since the Server needs to spawn some background tasks, we needed
 // to configure an Executor that can spawn !Send futures...
 #[derive(Clone, Copy, Debug)]
@@ -258,13 +431,13 @@ name_of_function().then...
 
 */
 
-
 struct SetTimeout {}
 
 impl SetTimeout {
-
-    pub fn new<F>(cb: &'static F, delayMS: u64) where F: Fn() {
-
+    pub fn new<F>(cb: &'static F, delayMS: u64)
+    where
+        F: Fn(),
+    {
         let delay = delayMS;
         tokio::task::spawn_local(async move {
             tokio::time::delay_for(Duration::from_millis(delay)).await;
@@ -285,7 +458,7 @@ impl<'a, F> Future for SetTimeout<'a, F> where F: Fn() {
     fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
 
         let delay = self.delayMS;
-        
+
 
         let mut is_done = *self.done.lock().unwrap();
 
@@ -304,6 +477,5 @@ impl<'a, F> Future for SetTimeout<'a, F> where F: Fn() {
             });
             std::task::Poll::Pending
         }
-   
     }
 }*/
